bf2e749635ca798b4f1715821b60111c62160ca7
Germán Boquizo Sánchez <xekevara@gmail.com>
2024-11-09 15:23:03 +0100

    chore:🔥 delete debug statement

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Actions\SendCoinbaseEmailAction;
 use Illuminate\Support\Facades\Route;
-
-Route::get('sandbox', static fn() => (new SendCoinbaseEmailAction())());
 
 Route::get('/', static fn() => Route::getFacadeApplication()?->version());
